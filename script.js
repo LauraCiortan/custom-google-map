04a1927cfea269a84558d51793c96dfe543c430d
Laura <ciortan.lauragabriela@yahoo.com>
2024-07-08 14:46:19 +0200

    Add custom marker

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat: 59.33258, lng: 18.0649 },
+    zoom: 12,
     mapId: "4fc65eb1725409dd",
+  });
+
+  const icon = {
+    url: "images/marker.png",
+    scaledSize: new google.maps.Size(55, 100),
+  };
+
+  const marker = new google.maps.Marker({
+    map: map,
+    position: { lat: 59.332585805600544, lng: 18.064583798565437 },
+    title: "welcome",
+    icon: icon,
   });
 }
